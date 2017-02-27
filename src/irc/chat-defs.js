e1aff5401900155b0f80ab992d4790534611df0f
CaffeinatedRat <caffeinatedrat@gmail.com>
2017-02-27 00:01:34 -0500

    Definition file added

---
+++ src/irc/chat-defs.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/*
+* @license
+* Copyright (c) 2016-2017, FarFromSubtle IT
+* All rights reserved.
+* Author: Ken Anderson <caffeinatedrat at gmail dot com>
+* @@@@@@@@@@@@@@@@
+* @@@@@@@@@@@@@@@@
+* @@@  @@@@@@  @@@
+* @@@  @@@@@@  @@@
+* @@@@@@@@@@@@@@@@
+* @@@          @@@
+* @@@          @@@
+* @@@@@@@@@@@@@@@@
+*    @@@@@@@@@@   
+*    @@@@@@@@@@   
+* @@@@@@@@@@@@@@@@
+* @@@@@@@@@@@@@@@@
+*    @@      @@   
+*    @@      @@   
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE AUTHOR AND CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+//Namespace declaration.
+var vga = vga || {};
+vga.irc = vga.irc || {};
+
+vga.irc.classes = {
+    owner: 'owner',
+    admin: 'admin',
+    mod: 'op',
+    guest: 'guest',
+    turbo: 'turbo',
+    shadow: 'shadow'
+};
+
+vga.irc.roles = {
+    owner: 32,
+    admin: 16,
+    mod: 8,
+    guest: 4,
+    turbo: 2,
+    shadow: 1
+};
+
+vga.irc.channelmodes = {
+    turbo: 1
+};
